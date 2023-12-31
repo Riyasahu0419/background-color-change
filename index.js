b2868e87af430e3d70e55809c18f0ee2f3d49c6f
Harendra <harendra@the-inkers.com>
2023-12-31 20:57:49 +0530

    multiple colors in background

---
+++ index.js
@@ -0,0 +1,31 @@
+const randomcolor = function(){
+    const hex = '0123456789ABCDEF';
+    let color = "#";
+    for(let i=0;i<6;i++){
+        color+=hex[Math.floor(Math.random()*16)];
+    }
+    return color;
+};
+
+let intervalid;
+const startchangingColor = function()
+{
+    if(!intervalid){
+
+        intervalid = setInterval(changeBgColor,1000);
+    }
+
+
+function changeBgColor(){
+    document.body.style.backgroundColor = randomcolor()
+}    
+};
+
+const stopChangingColor = function(){
+    clearInterval(intervalid);   
+    intervalid = null;                                
+}
+
+document.querySelector("#start").addEventListener('click',startchangingColor);
+
+document.querySelector("#stop").addEventListener('click',stopChangingColor);
